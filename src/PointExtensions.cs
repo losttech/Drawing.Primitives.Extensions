829878fabe69de4ad93c4c5effb888719c986df5
Victor Milovanov <lost@LOST-PC>
2020-01-21 15:21:35 -0800

    initial upload

---
+++ src/PointExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace System.Drawing.Primitives {
+    using System;
+    using System.Drawing;
+
+    public static class PointExtensions {
+        /// <summary>
+        /// Subtracts points as if they were vectors
+        /// </summary>
+        public static PointF Subtract(this PointF self, PointF other) => new PointF(self.X - other.X, self.Y - other.Y);
+        /// <summary>
+        /// Computes square of the distance to origin (0,0)
+        /// </summary>
+        public static float LengthSquared(this PointF self) => self.DotProduct(self);
+        /// <summary>
+        /// Computes dot product of point coordinates (assumes point to be a vector).
+        /// </summary>
+        public static float DotProduct(this PointF value, PointF other) => (value.X * other.X) + (value.Y * other.Y);
+        /// <summary>
+        /// Computes the distance to origin (0,0)
+        /// </summary>
+        public static float Length(this PointF self) => (float)Math.Sqrt(self.LengthSquared());
+        /// <summary>
+        /// Checks if two points are within a certain distance from each other.
+        /// </summary>
+        public static bool Equals(this PointF value, PointF other, double maxDistance) =>
+            value.Subtract(other).LengthSquared() < maxDistance * maxDistance;
+        /// <summary>
+        /// Scales the point as if it were a vector.
+        /// </summary>
+        public static PointF Scale(this PointF self, float scale) =>
+            new PointF(scale * self.X, scale * self.Y);
+    }
+}
---
+++ src/RectangleExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace System.Drawing.Primitives {
+    using System.Drawing;
+
+    public static class RectangleExtensions {
+        /// <summary>
+        /// Gets the central point of the rectangle
+        /// </summary>
+        public static PointF Center(this RectangleF rect) =>
+            new PointF(0.5f * (rect.Left + rect.Right), 0.5f * (rect.Top + rect.Bottom));
+
+        /// <summary>
+        /// Gets an intersection with other rectangle
+        /// </summary>
+        /// <param name="rect">Original rectangle</param>
+        /// <param name="other">Rectangle to intersect with</param>
+        public static RectangleF Intersection(this RectangleF rect, RectangleF other) {
+            rect.Intersect(other);
+            return rect;
+        }
+        /// <summary>
+        /// Calculates the area of the rectangle
+        /// </summary>
+        public static double Area(this RectangleF rect) => rect.Width * rect.Height;
+        /// <summary>
+        /// Get a rectangle, that is enlarged by the specified amount.
+        /// </summary>
+        /// <seealso cref="RectangleF.Inflate(RectangleF,float,float)"/>
+        public static RectangleF Inflated(this RectangleF rect, float x, float y) {
+            if (x < 0 && rect.Width < x)
+                return new RectangleF();
+            if (y < 0 && rect.Height < y)
+                return new RectangleF();
+            rect.Inflate(x, y);
+            return rect;
+        }
+        /// <summary>
+        /// Checks if rectangle's <see cref="RectangleF.Width"/> larger than its <see cref="RectangleF.Height"/>
+        /// </summary>
+        public static bool IsHorizontal(this RectangleF rect) => rect.Width > rect.Height;
+        /// <summary>
+        /// Gets the top left point of the rectangle
+        /// </summary>
+        public static PointF TopLeft(this RectangleF rect) => rect.Location;
+        /// <summary>
+        /// Gets the top right point of the rectangle
+        /// </summary>
+        public static PointF TopRight(this RectangleF rect) => new PointF(rect.Right, rect.Top);
+        /// <summary>
+        /// Gets the bottom right point of the rectangle
+        /// </summary>
+        public static PointF BottomRight(this RectangleF rect) => new PointF(rect.Right, rect.Bottom);
+        /// <summary>
+        /// Gets the bottom left point of the rectangle
+        /// </summary>
+        public static PointF BottomLeft(this RectangleF rect) => new PointF(rect.Left, rect.Bottom);
+        /// <summary>
+        /// Gets corner points
+        /// </summary>
+        public static PointF[] Corners(this RectangleF rect) =>
+            new[] { rect.TopLeft(), rect.TopRight(), rect.BottomRight(), rect.BottomLeft() };
+    }
+}
